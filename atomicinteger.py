4f2966bf3992c9dae8d44d010766b17fc718bfde
Боликов Роман Александрович <rabolikov@MStr.local>
2023-01-10 10:56:42 +1000

    Added AtomicInteger class

---
+++ atomicinteger.py
@@ -0,0 +1,33 @@
+import threading
+
+
+class AtomicInteger():
+    def __init__(self, value=0):
+        self._value = int(value)
+        self._lock = threading.Lock()
+
+    def increment_and_get(self):
+        with self._lock:
+            self._value += 1
+            return self._value
+
+    def decrement_and_get(self):
+        with self._lock:
+            self._value -= 1
+            return self._value
+
+    @property
+    def value(self):
+        with self._lock:
+            return self._value
+
+    @value.setter
+    def set_value(self, val):
+        with self._lock:
+            self._value = int(val)
+            return self._value
+
+
+if __name__ == '__main__':
+    ai = AtomicInteger(5)
+    print(ai.value)
